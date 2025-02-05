e117c03d65fefb7806aae5da15d7abe0e0f4f435
Charlesoluwafemi <76776186+Charlesoluwafemi@users.noreply.github.com>
2025-02-05 21:12:01 +0100

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['localhost'],
-  },
-  reactStrictMode: true, // Ensure this is here
-  experimental: {}, // Ensure there are no experimental features breaking the build
-};
+const nextConfig = {};
 
 export default nextConfig;
