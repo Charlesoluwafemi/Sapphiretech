8ade061ccd7fb3243f53b2dd4f3703dc6ac534d8
Charlesoluwafemi <76776186+Charlesoluwafemi@users.noreply.github.com>
2025-02-05 20:35:48 +0100

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['localhost'], // Allow localhost for image sources
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['localhost'], // Allow localhost for image sources
+  },
+  reactStrictMode: true, // Optional: Enforce best practices
+};
+
+export default nextConfig;
